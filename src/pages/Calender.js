846d9eae4ec4be52c8e89b7338ecbe7efae8fef8
vedant kangde <vedant.kangde@gmail.com>
2021-04-13 19:09:51 +0530

    Added my project

---
+++ src/pages/Calender.js
@@ -0,0 +1,22 @@
+import React,{useState} from 'react';
+import MyCalender from '../components/Calender'
+
+
+const Calender = () => {
+
+    
+ 
+
+  
+    return (
+        <div>
+          
+         <MyCalender/>
+         
+        </div>
+    );
+}
+
+export default Calender;
+
+
